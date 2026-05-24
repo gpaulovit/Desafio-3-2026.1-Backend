0457306e7c3e4045a776129a395baa4e5c177e8b
Paulo Vitor Gomes <bpaulovitorgomes@gmail.com>
2026-05-24 19:37:59 -0300

    Resolução do desafio utilizando um serviço de API

--- src/script.js
+++ src/script.js
@@ -1,23 +1,54 @@
-//função de mudar imagem pelo id e pela url
 function changeImage(id, url) {
   document.getElementById(id).src = url;
 }
-//função de mudar texto pelo id e pelo texto
+
 function changeText(id, text) {
   document.getElementById(id).innerText = text;
 }
 
-// Daqui para baixo voce ira escrever
-// o código para resolver o desafio
+let pokemonList = [];
+let currentIndex = 0;
+
+async function loadPokemonList() {
+  const response = await fetch(
+    "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1025"
+  );
+  const data = await response.json();
+  pokemonList = data.results; 
+  showPokemon(currentIndex);  
+}
+
+async function showPokemon(index) {
+  const pokemon = pokemonList[index];
+
+  const response = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
+  );
+  const data = await response.json();
+
+  const nome = data.name.charAt(0).toUpperCase() + data.name.slice(1);
+
+  changeText("name", `#${data.id} - ${nome}`);
+  changeImage("img_sprite_front_default", data.sprites.front_default);
+}
 
 function previousPokemon() {
-  alert("Pokemon Anterior");
-  //abra o terminal em inspecionar no chrome para visualizar
-  console.log("Pokemon Anterior");
+  if (currentIndex === 0) {
+    currentIndex = pokemonList.length - 1;
+  } else {
+    currentIndex--;
+  }
+  showPokemon(currentIndex);
 }
 
 function nextPokemon() {
-  alert("Pokemon Seguinte");
-  //abra o terminal em inspecionar no chrome para visualizar
-  console.log("Pokemon Seguinte");
+  if (currentIndex === pokemonList.length - 1) {
+    currentIndex = 0;
+  } else {
+    currentIndex++;
+  }
+  showPokemon(currentIndex);
 }
+
+loadPokemonList();
+
